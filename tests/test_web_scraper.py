d1db5e24afc312a108203c905f1db920d504715c
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-01 18:54:03 +0200

    test: added a test to check of the function get_citations_needed_count returns the correct count

--- tests/test_web_scraper.py
+++ tests/test_web_scraper.py
@@ -1,5 +1,18 @@
-from web_scraper import __version__
+import pytest
+from web_scraper import __version__, get_citations_needed_count
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+@pytest.mark.parametrize(
+    "url, count",
+    [
+        ("https://en.wikipedia.org/wiki/Uncertainty_principle", 1),
+        ("https://en.wikipedia.org/wiki/Albert_Einstein", 2),
+        ("https://en.wikipedia.org/wiki/Quantum_mechanics",0),
+    ],
+)
+def test_get_citations_needed_count(url, count):
+    assert get_citations_needed_count(url) == count
