1c5c571805171168fc6640efa6b7c3c00af509ea
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-01 18:58:30 +0200

    feat: added the get_citatoins_needed_report function

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -6,3 +6,10 @@ def get_citations_needed_count(URL):
     content = BS(page.content, "html.parser")
     citations = content.find_all( "span", string="citation needed")
     return len(citations)
+
+def get_citations_needed_report(URL):
+    page = requests.get(URL)
+    content = BS(page.content, "html.parser")
+    citations = content.find_all( "span", string="citation needed")
+    report = "\n".join([span.parent.parent.parent.parent.text for span in citations])
+    return report
